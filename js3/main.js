27078aacacd0e08b760d96ca66ee7b957ad30b27
Raiquen248 <raiquenn@hotmail.com>
2023-10-03 22:31:54 -0300

    js3

---
+++ js3/main.js
@@ -0,0 +1,96 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const comprarBtn = document.getElementById("comprarBtn");
+    const tipoInvitacion = document.getElementById("tipoInvitacion");
+    const opcionInvitacion = document.getElementById("opcionInvitacion");
+    const tablaInvitacionesBody = document.getElementById("tabla-invitaciones-body");
+    const subtotalElement = document.getElementById("subtotal");
+    const descuentoElement = document.getElementById("descuento");
+    const totalElement = document.getElementById("total");
+
+    let subtotal = 0;
+    let invitacionesCompradas = [];
+
+    comprarBtn.addEventListener("click", () => {
+        const invitacion = tipoInvitacion.value;
+        const precioBase = obtenerPrecioBase(invitacion);
+        const opcion = parseInt(opcionInvitacion.value);
+
+        const invitacionNueva = new Invitacion(invitacion, precioBase, opcion);
+
+        const costoTotal = invitacionNueva.precioTotal;
+        subtotal += costoTotal;
+
+        const fila = document.createElement("tr");
+        fila.innerHTML = `
+            <td>${invitacionNueva.tipo}</td>
+            <td>$${precioBase}</td>
+            <td>${opcion}</td>
+            <td>$${costoTotal}</td>
+        `;
+        tablaInvitacionesBody.appendChild(fila);
+
+        subtotalElement.textContent = `Subtotal: $${subtotal}`;
+
+        invitacionesCompradas.push(invitacionNueva);
+
+        if (subtotal > 5000) {
+            const descuento = calcularPorcentaje(subtotal, 20);
+            subtotal -= descuento;
+            descuentoElement.textContent = `Descuento: -$${descuento}`;
+        } else {
+            descuentoElement.textContent = "Descuento: $0";
+        }
+
+        totalElement.textContent = `Total: $${subtotal}`;
+    });
+
+    class Invitacion {
+        constructor(tipo, precioBase, opcion) {
+            this.tipo = tipo;
+            this.precioBase = precioBase;
+            this.opcion = opcion;
+            this.precioTotal = this.calcularPrecioTotal();      
+        }
+
+        calcularPrecioTotal() {
+            return this.precioBase * this.opcion;
+        }
+    }
+
+    function calcularPorcentaje(valor, porcentaje) {
+        return (valor * porcentaje) / 100;
+    }
+
+    const obtenerPrecioBase = (tipo) => {
+        switch (tipo) {
+            case "BODA":
+                return 2000;
+            case "QUINCE":
+                return 1500;
+            case "INFANTIL":
+                return 1000;
+            default:
+                return 0;
+        }
+    };
+    const resetBtn = document.getElementById("resetBtn");
+
+resetBtn.addEventListener("click", () => {
+    while (tablaInvitacionesBody.firstChild) {
+        tablaInvitacionesBody.removeChild(tablaInvitacionesBody.firstChild);
+    }
+
+    // Restablece el subtotal, descuento y total a 0
+    subtotal = 0;
+    descuentoElement.textContent = "Descuento: $0";
+    subtotalElement.textContent = "Subtotal: $0";
+    totalElement.textContent = "Total: $0";
+
+    // Limpia el array de invitaciones compradas
+    invitacionesCompradas = [];
+});
+
+});
+
+
+
